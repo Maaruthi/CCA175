a479f2cf904728d37ff5918cca5344b078c49c2a
Maaruthi <naidu1111@gmail.com>
2016-09-11 12:19:00 -0600

    Uploaded all the files till day

---
+++ code/spark/python/simpleApp.py
@@ -0,0 +1,9 @@
+from pyspark import SparkContext, SparkConf
+
+conf = SparkConf().setAppName("Ganesha")
+sc = SparkContext(conf=conf)
+
+dataRDD = sc.textFile("/user/cloudera/sqoop_import/departments")
+for line in dataRDD.collect():
+	print(line)
+dataRDD.saveAsTextFile("/user/cloudera/pyspark/department_python")
